2628ad665942dd931e68ce15b4a593b05d3499ec
math-lover31415 <pradyumn3.1415@gmail.com>
2023-10-07 15:46:34 +0530

    model

---
+++ ml/dataset_gen.py
@@ -0,0 +1,63 @@
+import os
+import pickle
+import mediapipe as mp
+import cv2
+import numpy as np
+
+hands = mp.solutions.hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+data = []
+signs = []
+
+# Define the maximum number of features (landmark coordinates) per sample
+max_features_per_sample = 171
+
+for dir_name in os.listdir("./data"):
+    counter = 0
+    for img_path in os.listdir(os.path.join('./data', dir_name)):
+        if counter >= 1200:  # Limit to 10 samples per class
+            break
+
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        img = cv2.imread(os.path.join('./data', dir_name, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+            max_x = max(x_)
+            max_y = max(y_)
+
+            for i in range(len(x_)):
+                data_aux.append(x_[i] / max_x)
+                data_aux.append(y_[i] / max_y)
+
+            # Pad or truncate data_aux to have max_features_per_sample elements
+            if len(data_aux) < max_features_per_sample:
+                data_aux += [0] * (max_features_per_sample - len(data_aux))
+            elif len(data_aux) > max_features_per_sample:
+                data_aux = data_aux[:max_features_per_sample]
+
+            data.append(data_aux)
+            signs.append(dir_name)
+
+        counter += 1
+
+# Convert data and signs to NumPy arrays
+data = np.array(data)
+signs = np.array(signs)
+
+# Save the generated dataset
+f = open('./data.pickle', 'wb')
+pickle.dump({'data': data, 'signs': signs}, f)
+f.close()
---
+++ ml/model.py
@@ -0,0 +1,28 @@
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+data_dict = pickle.load(open('./data.pickle', 'rb'))
+
+data = np.asarray(data_dict['data'])
+signs = np.asarray(data_dict['signs'])
+
+x_train, x_test, y_train, y_test = train_test_split(data, signs, test_size=0.2, shuffle=True, stratify=signs)
+
+model = RandomForestClassifier()
+
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
+score = accuracy_score(y_predict, y_test)
+
+print('{}% of samples were classified correctly !'.format(score * 100))
+
+f = open('model.pickle', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
